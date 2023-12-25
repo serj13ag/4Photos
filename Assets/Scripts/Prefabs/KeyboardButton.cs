36e8b09b7e41e717d8f262fcc637ea76b214923d
Sergey Agafonov <serj13serj@yandex.ru>
2023-12-25 19:50:34 +0400

    add keyboard button prefab

---
+++ Assets/Scripts/Prefabs/KeyboardButton.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Prefabs
+{
+    public class KeyboardButton : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _text;
+
+        public void Init(string character)
+        {
+            _text.text = character;
+        }
+    }
+}
