c86f511d7bc263ab58832ee277fa8aab29884f47
Sergey Agafonov <serj13serj@yandex.ru>
2023-12-25 19:49:53 +0400

    add array extensions with shuffle method

---
+++ Assets/Scripts/Extensions/ArrayExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static void Shuffle<T>(this T[] array, Random rnd)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = rnd.Next(n--);
+                (array[n], array[k]) = (array[k], array[n]);
+            }
+        }
+    }
+}
