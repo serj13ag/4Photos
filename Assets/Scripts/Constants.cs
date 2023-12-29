980a44b098c11b779e8a6547b34ae5dcb5a36e70
Sergey Agafonov <serj13serj@yandex.ru>
2023-12-29 18:13:45 +0400

    add static data validation

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -3,7 +3,9 @@
 public static class Constants
 {
     public const string AlphabetCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public const int NumberOfImages = 4;
     public const int NumberOfKeyboardButtons = 14;
+    public const int MaxNumberOfWordCharacters = 7;
 
     public static Color EmptyButtonColor = Color.white;
     public static Color FilledByKeyboardButtonColor = Color.yellow;
--- Assets/Scripts/ScriptableObjects/LevelStaticData.cs
+++ Assets/Scripts/ScriptableObjects/LevelStaticData.cs
@@ -5,13 +5,36 @@ namespace ScriptableObjects
     [CreateAssetMenu(fileName = "LevelData", menuName = "Data/LevelData")]
     public class LevelStaticData : ScriptableObject
     {
-        public string LevelName;
+        public string Code;
         public Sprite[] Images;
         public string Word;
 
         private void OnValidate()
         {
-            LevelName = name;
+            Code = name;
+
+            if (Images.Length < Constants.NumberOfImages)
+            {
+                Debug.LogError($"{nameof(LevelStaticData)}-{Code}: {nameof(Images)} count is less than {Constants.NumberOfImages}");
+            }
+
+            for (int i = 0; i < Images.Length; i++)
+            {
+                if (Images[i] == null)
+                {
+                    Debug.LogError($"{nameof(LevelStaticData)}-{Code}: {nameof(Images)}[{i}] is null");
+                }
+            }
+
+            if (Word.Length == 0)
+            {
+                Debug.LogError($"{nameof(LevelStaticData)}-{Code}: {nameof(Word)} is empty");
+            }
+
+            if (Word.Length > Constants.MaxNumberOfWordCharacters)
+            {
+                Debug.LogError($"{nameof(LevelStaticData)}-{Code}: {nameof(Word)} has more than {Constants.MaxNumberOfWordCharacters} characters");
+            }
         }
     }
 }
