b4e18a174849dfd6fe62d8b869cca549f1ae6509
Sergey Agafonov <serj13serj@yandex.ru>
2024-01-08 18:58:52 +0400

    change colors

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -8,7 +8,7 @@ public static class Constants
     public const int MaxNumberOfWordCharacters = 7;
 
     public static Color EmptyButtonColor = Color.white;
-    public static Color FilledByKeyboardButtonColor = Color.yellow;
-    public static Color FilledByHintButtonColor = Color.green;
-    public static Color WrongCharacterButtonColor = Color.red;
+    public static Color FilledByKeyboardButtonColor = new Color32(233, 196, 106, 255);
+    public static Color FilledByHintButtonColor = new Color32(42, 157, 143, 255);
+    public static Color WrongCharacterButtonColor = new Color32(231, 111, 81, 255);
 }
