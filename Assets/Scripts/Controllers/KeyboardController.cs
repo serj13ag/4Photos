849a898e6d6f4af1a7eaa56a4e2e353a2e8bc1fe
Sergey Agafonov <serj13serj@yandex.ru>
2024-01-17 20:46:36 +0400

    add keyboard controller

---
+++ Assets/Scripts/Controllers/KeyboardController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Extensions;
+using Prefabs;
+using Services;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Controllers
+{
+    public class KeyboardController : MonoBehaviour
+    {
+        [SerializeField] private MainController _mainController;
+        [SerializeField] private WordController _wordController;
+
+        [SerializeField] private KeyboardButton _keyboardButtonPrefab;
+        [SerializeField] private Transform _keyboardContainer;
+
+        [SerializeField] private Button _hintHideWrongKeyboardCharacterButton;
+
+        private RandomService _randomService;
+
+        private KeyboardButton[] _keyboardButtons;
+
+        private void OnEnable()
+        {
+            _hintHideWrongKeyboardCharacterButton.onClick.AddListener(OnHintHideWrongKeyboardCharacterButtonClicked);
+        }
+
+        private void OnDisable()
+        {
+            _hintHideWrongKeyboardCharacterButton.onClick.RemoveListener(OnHintHideWrongKeyboardCharacterButtonClicked);
+        }
+
+        public void Init(RandomService randomService)
+        {
+            _randomService = randomService;
+        }
+
+        public void CreateKeyboardButtons(IReadOnlyList<char> randomCharacters)
+        {
+            _keyboardButtons = new KeyboardButton[randomCharacters.Count];
+
+            for (int i = 0; i < randomCharacters.Count; i++)
+            {
+                KeyboardButton keyboardButton = Instantiate(_keyboardButtonPrefab, _keyboardContainer);
+                keyboardButton.Init(randomCharacters[i], _wordController);
+                _keyboardButtons[i] = keyboardButton;
+            }
+        }
+
+        public void ClearKeyboardButtons()
+        {
+            _keyboardContainer.DestroyAllChildren();
+        }
+
+        private void OnHintHideWrongKeyboardCharacterButtonClicked()
+        {
+            foreach (KeyboardButton keyboardButton in _keyboardButtons.OrderBy(_ => _randomService.Random.Next()))
+            {
+                if (!keyboardButton.IsHided && !_mainController.AnswerWord.Contains(keyboardButton.Character))
+                {
+                    keyboardButton.Hide();
+                    return;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Controllers/MainController.cs
+++ Assets/Scripts/Controllers/MainController.cs
@@ -1,12 +1,7 @@
-using System.Collections.Generic;
-using System.Linq;
-using Extensions;
-using Prefabs;
 using ScriptableObjects;
 using Services;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Controllers
 {
@@ -17,19 +12,14 @@ namespace Controllers
         [SerializeField] private CoinsController _coinsController;
         [SerializeField] private ImagesController _imagesController;
         [SerializeField] private WordController _wordController;
-
-        [SerializeField] private KeyboardButton _keyboardButtonPrefab;
-        [SerializeField] private Transform _keyboardContainer;
+        [SerializeField] private KeyboardController _keyboardController;
 
         [SerializeField] private TMP_Text _levelNumberText;
 
-        [SerializeField] private Button _hintHideWrongKeyboardCharacterButton;
-
         private RandomService _randomService;
-
         private int _currentLevelIndex;
-        private string _answerWord;
-        private KeyboardButton[] _keyboardButtons;
+
+        public string AnswerWord { get; private set; }
 
         private void Start()
         {
@@ -40,34 +30,26 @@ namespace Controllers
             _currentLevelIndex = 0;
 
             _wordController.Init(_randomService);
+            _keyboardController.Init(_randomService);
+
             _coinsController.SetCoinsAmount(Constants.InitialCoinsAmount);
 
-            InitLevelWithCurrentIndex();
+            CreateLevelWithCurrentIndex();
         }
 
-        private void OnEnable()
-        {
-            _hintHideWrongKeyboardCharacterButton.onClick.AddListener(OnHintHideWrongKeyboardCharacterButtonClicked);
-        }
-
-        private void OnDisable()
-        {
-            _hintHideWrongKeyboardCharacterButton.onClick.RemoveListener(OnHintHideWrongKeyboardCharacterButtonClicked);
-        }
-
-        private void InitLevelWithCurrentIndex()
+        private void CreateLevelWithCurrentIndex()
         {
             LevelStaticData currentLevelStaticData = _levelsStaticData[_currentLevelIndex];
 
-            _answerWord = currentLevelStaticData.Word.ToUpper();
-            char[] answerChars = _answerWord.ToCharArray();
+            AnswerWord = currentLevelStaticData.Word.ToUpper();
+            char[] answerChars = AnswerWord.ToCharArray();
             char[] charactersForKeyboard = _randomService.GetCharactersForKeyboard(answerChars, Constants.NumberOfKeyboardButtons);
-
-            _levelNumberText.text = (_currentLevelIndex + 1).ToString();
 
             _imagesController.CreateImageButtons(currentLevelStaticData.Images, Constants.NumberOfInitiallyOpenedImages);
             _wordController.CreateWordButtons(answerChars);
-            CreateKeyboardButtons(charactersForKeyboard);
+            _keyboardController.CreateKeyboardButtons(charactersForKeyboard);
+
+            UpdateLevelNumberText();
         }
 
         public void ChangeLevelToNext()
@@ -79,38 +61,19 @@ namespace Controllers
             }
 
             Cleanup();
-            InitLevelWithCurrentIndex();
-        }
-
-        private void OnHintHideWrongKeyboardCharacterButtonClicked()
-        {
-            foreach (KeyboardButton keyboardButton in _keyboardButtons.OrderBy(_ => _randomService.Random.Next()))
-            {
-                if (!keyboardButton.IsHided && !_answerWord.Contains(keyboardButton.Character))
-                {
-                    keyboardButton.Hide();
-                    return;
-                }
-            }
-        }
-
-        private void CreateKeyboardButtons(IReadOnlyList<char> randomCharacters)
-        {
-            _keyboardButtons = new KeyboardButton[randomCharacters.Count];
-
-            for (int i = 0; i < randomCharacters.Count; i++)
-            {
-                KeyboardButton keyboardButton = Instantiate(_keyboardButtonPrefab, _keyboardContainer);
-                keyboardButton.Init(randomCharacters[i], _wordController);
-                _keyboardButtons[i] = keyboardButton;
-            }
+            CreateLevelWithCurrentIndex();
         }
 
         private void Cleanup()
         {
             _imagesController.ClearImages();
             _wordController.ClearWordButtons();
-            _keyboardContainer.DestroyAllChildren();
+            _keyboardController.ClearKeyboardButtons();
+        }
+
+        private void UpdateLevelNumberText()
+        {
+            _levelNumberText.text = (_currentLevelIndex + 1).ToString();
         }
     }
 }
